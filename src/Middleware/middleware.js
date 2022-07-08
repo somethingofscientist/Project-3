e6dccda0e9cd3d8fa312bb8fb7253e3be0d32a5d
sahilpayla <1shraddhanand@gmail.com>
2022-07-08 16:53:32 +0530

    user

--- src/Middleware/middleware.js
+++ src/Middleware/middleware.js
@@ -9,7 +9,7 @@ const Authenticate = function (req, res, next) {
     try {
         let token = req.headers["x-api-key"];
         if (!token) return res.status(401).send({ status: false, msg: "token must be present in the request header" });
-        jwt.verify(token, "project_3",function(err,decodedToken){
+        jwt.verify(token, "project_3", function(err,decodedToken){
             if(err)  return res.status(401).send({ status: false, msg: "token is not valid" });
             req.newUser = decodedToken.userId 
         });
@@ -26,20 +26,20 @@ const Authorisation = async function (req, res, next) {
         let bookId = req.params.bookId
         if (bookId) {
             // book update or delete 
-            let isValid = mongoose.Types.ObjectId.isValid(bookId)
+            let isValid = mongoose.Types.ObjectId.isValid(bookId) 
             if (!isValid) return res.status(400).send({ status: false, message: "bookId is not valid" })
             let userId = await bookModel.findOne({ _id: bookId }).select({ userId: 1, _id: 0 })
             if(!userId) return res.status(400).send({ status: false, msg: 'Please enter valid book ID' })
             let newAuth = userId.userId.valueOf()
             if (newAuth != userLoggedIn) return res.status(403).send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
         }
-        else {
-            // book creation
-            let requestUser = req.body.userId
-            let isValid = mongoose.Types.ObjectId.isValid(requestUser)
-            if (!isValid) return res.status(400).send({ status: false, message: "bookId is not valid" })
-            if (requestUser != userLoggedIn) return res.status(403).send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
-        }
+        // else {
+        //     // book creation
+        //     let requestUser = req.body.userId
+        //     let isValid = mongoose.Types.ObjectId.isValid(requestUser)
+        //     if (!isValid) return res.status(400).send({ status: false, message: "bookId is not valid" })
+        //     if (requestUser != userLoggedIn) return res.status(403).send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
+        // }
         next()
     } catch (err) {
         res.status(500).send({ status: false, msg: err.message })
--- src/index.js
+++ src/index.js
@@ -7,16 +7,15 @@ const app = express();
 app.use(bodyParser.json()); 
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-
 mongoose.connect("mongodb+srv://ShivamKoushik:s%40H9663334444@cluster0.k1qkf.mongodb.net/Group70Databasenew?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
+// .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 app.use('/', route);
 
 
 app.listen(process.env.PORT || 3001, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3001))
+    // console.log('Express app running on port ' + (process.env.PORT || 3001))
 });
